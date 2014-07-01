27ac18ad0cf224b1db48618d293308aacf33d1fe
thehappyman <1246505492@qq.com>
2014-07-01 18:25:29 +0800

    xiugai

---
+++ js/example.js
@@ -0,0 +1,18 @@
+/**
+ * Created by M.Jin on 14-7-1.
+ */
+window.onload = function(){
+    var testdiv = document.getElementById("testdiv");
+    var para = document.createElement("p");
+    var txt1 = document.createTextNode("This is");
+    var elph = document.createElement("em");
+    var txt2 = document.createTextNode("my");
+    var txt3 = document.createTextNode("content.");
+
+    elph.appendChild(txt2);
+    para.appendChild(txt1);
+    para.appendChild(elph);
+    para.appendChild(txt3);
+
+    testdiv.appendChild(para);
+}

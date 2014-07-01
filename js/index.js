187c9816e66f2d10175b87cfa277f01464add49b
thehappyman <1246505492@qq.com>
2014-07-01 12:04:26 +0800

    take directory

--- js/index.js
+++ js/index.js
@@ -1,3 +1,33 @@
 /**
  * Created by M.Jin on 14-7-1.
  */
+//dom操作中最常用的五个方法如下
+//document.getElementById();
+//document.getElementsByClassName();
+//document.getElementsByTagName();
+//document.getAttribute();
+//document.setAttribute();
+
+//实现自己的getElementsByClassName()
+
+function getElementsByClassName(node ,classname){
+    if(node.getElementsByClassName){
+        //使用现有方法
+        return node.getElementsByClassName(classname);
+    }else{
+        var results = new Array();
+        var elems = node.getElementsByTagName("*");
+        for(var i=0;i<elems.length;i++){
+            if(elems[i].className.indexOf(classname) != -1){
+                results[results.length] = elems[i];
+            }
+        }
+        return results;
+    }
+}
+
+//获取属性
+var paras = document.getElementsByTagName("p");
+for(var i=0;i<paras.length;i++){
+    alert(paras[i].getAttribute("title"));
+}

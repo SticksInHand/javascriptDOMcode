63cbbe53d8eb66901c0b43bb4dec1508e49eaf1b
thehappyman <1246505492@qq.com>
2014-07-02 11:39:35 +0800

    ajax

---
+++ js/addLoadEvent.js
@@ -0,0 +1,14 @@
+/**
+ * Created by M.Jin on 14-7-2.
+ */
+function addLoadEvent(func){
+    var oldonload = window.onload;
+    if(typeof window.onload != 'function'){
+        window.onload = func;
+    }else{
+        window.onload = function(){
+            oldonload();
+            func();
+        }
+    }
+}
